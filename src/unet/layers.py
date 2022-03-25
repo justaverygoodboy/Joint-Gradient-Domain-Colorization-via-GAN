f644d333fcd71d260a5c292b7fcb859cad8cde16
justaverygoodboy <1206146168@qq.com>
2022-03-25 09:28:25 +0800

    change down arch to conv-bn-conv-gelu

--- src/unet/layers.py
+++ src/unet/layers.py
@@ -13,31 +13,38 @@ class unetConv2(nn.Module):
         self.padding = padding
         s = stride
         p = padding
-        if is_batchnorm:
-            for i in range(1, n + 1): #两层 Conv-BN-ReLU
-                conv = nn.Sequential(nn.Conv2d(in_size, out_size, ks, s, p), # (w-kz)+2p+1，这里尺寸不变
-                                     nn.BatchNorm2d(out_size),
-                                     nn.ReLU(inplace=True), )
-                setattr(self, 'conv%d' % i, conv)
-                in_size = out_size
+        # if is_batchnorm:
+        #     for i in range(1, n + 1): #两层 Conv-BN-ReLU
+        #         conv = nn.Sequential(nn.Conv2d(in_size, out_size, ks, s, p), # (w-kz)+2p+1，这里尺寸不变
+        #                              nn.BatchNorm2d(out_size),
+        #                              nn.ReLU(inplace=True), )
+        #         setattr(self, 'conv%d' % i, conv)
+        #         in_size = out_size
 
-        else:
-            for i in range(1, n + 1):
-                conv = nn.Sequential(nn.Conv2d(in_size, out_size, ks, s, p),
-                                     nn.ReLU(inplace=True), )
-                setattr(self, 'conv%d' % i, conv)
-                in_size = out_size
-
+        # else:
+        #     for i in range(1, n + 1):
+        #         conv = nn.Sequential(nn.Conv2d(in_size, out_size, ks, s, p),
+        #                              nn.ReLU(inplace=True), )
+        #         setattr(self, 'conv%d' % i, conv)
+        #         in_size = out_size
+        conv = nn.Sequential(
+            nn.Conv2d(in_size,out_size,ks,s,p),
+            nn.BatchNorm2d(out_size),
+            nn.Conv2d(out_size,out_size,ks,s,p),
+            nn.GELU(inplace=True)
+            )
+        setattr(self,'conv',conv)
         # initialise the blocks
         for m in self.children():
             init_weights(m, init_type='kaiming')
 
     def forward(self, inputs):
         x = inputs
-        for i in range(1, self.n + 1):
-            conv = getattr(self, 'conv%d' % i)
-            x = conv(x)
-
+        # for i in range(1, self.n + 1):
+        #     conv = getattr(self, 'conv%d' % i)
+        #     x = conv(x)
+        conv = getattr(self,'conv')
+        x = conv(x)
         return x
 
 
